afd2ab1a28467816a7bf2fdb62a843b06b567d86
hillreinermichael <hillreiner.michael@googlemail.com>
2026-09-02 18:09:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitness-coach-v9';
+const CACHE='fitness-coach-v10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon.ico','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
